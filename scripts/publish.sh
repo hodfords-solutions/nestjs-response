4cd47b4868c0c3f2242f3af4eafd4c978092bf4d
phuong-hodfords <phuong.vt@hodfords.com>
2024-08-27 12:14:53 +0700

    chore: update ci

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,7 @@
 npm view @hodfords/nestjs-response@"$(node -p "require('./package.json').version")" version && echo "Package is already published" && exit 0 || true
 npm install
 npm run build
+cp package.json dist/libs
 cp README.md dist
 cd dist
 npm publish --access public
